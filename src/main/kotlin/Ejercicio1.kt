a678c5aa106d0347e6833eb7d73e8ce198d414e8
Francisco_Diaz <pacodiaz008@gmail.com>
2023-09-24 15:22:38 +0200

    El ejercicio 2 de la unidad 2 de Android

---
+++ src/main/kotlin/Ejercicio1.kt
@@ -0,0 +1,19 @@
+class Ejercicio1 {
+    /*Ejercicio 1
+a) Implementar las clases SerVivo y Humano en Kotlin.
+El método mayor DEBE DEVOLVER un objeto de la clase SerVivo.
+En el caso de la versión del método mayor que está en la clase SerVivo va
+a devolver al SerVivo (de entre los dos que se comparan) de MAYOR EDAD.
+En el caso de la versión del método que se encuentra en la clase Humano
+DEBERÁ DEVOLVER al humano (de entre los dos que se comparan) cuya
+edad sea mayor. En caso de tener la misma edad, devolverá al humano
+cuyo nombre sea más largo.
+El método toString() DEBE devolver un String que contenga TODOS los
+datos del objeto.
+Para implementar el método equals DEBES SABER QUE:
+• Dos seres vivos se consideran iguales si tienen la misma edad
+• Dos humanos se consideran iguales si tienen la misma edad y el
+mismo nombre.*/
+
+
+}
